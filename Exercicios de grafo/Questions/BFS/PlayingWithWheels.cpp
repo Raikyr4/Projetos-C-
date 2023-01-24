bbc658b71b3eecfb7d172aa81301c51fba8e799a
Raiky Sahb <raikypsahb12@gmail.com>
2023-01-24 17:46:35 -0300

    .

---
+++ Exercicios de grafo/Questions/BFS/PlayingWithWheels.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int  N;
+
+int main()
+{
+    
+    
+    while (cin>>N && N)
+    {
+        for(int i =0; i < N; i++)
+        {
+            
+        }
+    }
+    
+}
